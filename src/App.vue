5217ae051c694ca78e78cc24b3e83b5ec2343ceb
Eren Küçük <erenkucuk2002@gmail.com>
2022-12-11 19:52:30 +0300

    sos

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="app">
+    <div class="flex button">
+      <button>S</button>
+      <button>O</button>
+    </div>
+    <div class="flex">
+      <div v-for="n in 10" :key="n" class="hello" @click="getKey(n)"></div>
+    </div>
+    <div class="flex">
+      <div v-for="n in 10" :key="n" class="hello" @click="getKey(n)"></div>
+    </div>
+    <div class="flex">
+      <div v-for="n in 10" :key="n" class="hello" @click="getKey(n)"></div>
+    </div>
+    <div class="flex">
+      <div v-for="n in 10" :key="n" class="hello" @click="getKey(n)"></div>
+    </div>
+    <div class="flex">
+      <div v-for="n in 10" :key="n" class="hello" @click="getKey(n)"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  methods: {
+    getKey: (n) => {
+      //key
+      console.log(n);
+    },
+  },
+};
+</script>
+55
+
+<style>
+.app {
+  flex-wrap: wrap;
+  margin-top: 4rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.flex {
+  display: flex;
+}
+
+.hello {
+  width: 100px;
+  height: 100px;
+  border: 1px solid #000;
+}
+.button {
+  margin-bottom: 1rem;
+  gap: 1rem;
+  font-size: 2rem;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello" @click="getKey($event)">{{ n }}</div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {};
+  },
+  props: {
+    msg: String,
+  },
+  methods: {
+    getKey: (event) => {
+      //key
+      console.log(event.target);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  width: 100px;
+  height: 100px;
+  border: 1px solid #000;
+}
+</style>
